4ebe045485e47303f7310d3283dceb69489816ed
SmirnovIvan1 <iva2611@yandex.ru>
2014-11-27 23:16:13 +0300

    №2
    Добавил загрузку из инета картинок
    Добавил картинку для движения
    Думаю можно добавить кнопку настройки (в командной строке) как мы
    делали в какой то лабе и через нее переходить на игру на счет и тд

--- app/src/main/java/com/example/igra/app/MainActivity.java
+++ app/src/main/java/com/example/igra/app/MainActivity.java
@@ -6,8 +6,11 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
-
+import com.nostra13.universalimageloader.cache.memory.impl.LruMemoryCache;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 import java.util.Random;
 
 
@@ -22,6 +25,15 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageLoaderConfiguration config = new
+        ImageLoaderConfiguration.Builder(getApplicationContext()).memoryCache(new LruMemoryCache(32 * 1024 * 1024))
+                .memoryCacheSize(32 * 1024 * 1024)
+                .discCacheSize(100 * 1024 * 1024)
+                .discCacheFileCount(1000).build();
+        ImageLoader.getInstance().init(config);
+        ImageLoader.getInstance().displayImage(getString(R.string.url_left), (ImageView)  findViewById(R.id.button));
+        ImageLoader.getInstance().displayImage(getString(R.string.url_down), (ImageView)  findViewById(R.id.button2));
+        ImageLoader.getInstance().displayImage(getString(R.string.url_right), (ImageView)  findViewById(R.id.button3));
         sharedPreferences = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
         final TextView text2 = (TextView) findViewById(R.id.textView2);
         rand = new Random();
@@ -29,6 +41,15 @@ public class MainActivity extends ActionBarActivity {
             @Override
             public void onClick(View v) {
                 rand();
+                if (zagad==1){
+                    ImageLoader.getInstance().displayImage(getString(R.string.url_left), (ImageView)  findViewById(R.id.imageView));
+                } else {
+                    if (zagad==2){
+                        ImageLoader.getInstance().displayImage(getString(R.string.url_down), (ImageView)  findViewById(R.id.imageView));
+                    }else {
+                        ImageLoader.getInstance().displayImage(getString(R.string.url_right), (ImageView)  findViewById(R.id.imageView));
+                    }
+                }
             }
         });
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
